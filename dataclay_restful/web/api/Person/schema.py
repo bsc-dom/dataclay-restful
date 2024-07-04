b5116f27774ffd77d7e247176fb5098fe3ff8f54
Marc Monfort <marc.monfort@gmail.com>
2024-07-04 20:29:15 +0200

    Update Person schema and add new routes

--- dataclay_restful/web/api/Person/schema.py
+++ dataclay_restful/web/api/Person/schema.py
@@ -1,1 +1,34 @@
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import AliasGenerator, BaseModel, ConfigDict, create_model
+from typing import Any, Dict, Optional, Type
+from uuid import UUID
 
+from dataclay.contrib.modeltest.family import Person, Dog
+from dataclay.dataclay_object import DC_PROPERTY_PREFIX
+
+
+# TODO: Create PersonBase from Person class dynamically
+# def create_pydantic_model_from_class(cls: Type) -> BaseModel:
+#     annotations = cls.__annotations__
+#     fields = {k: (v, None) for k, v in annotations.items()}
+#     pydantic_model = create_model(
+#         cls.__name__ + "Model", **fields, __config__={"arbitrary_types_allowed": True}
+#     )
+#     return pydantic_model
+
+
+# PersonBase = create_pydantic_model_from_class(Person)
+
+
+class PersonBase(BaseModel):
+
+    model_config = ConfigDict(
+        alias_generator=AliasGenerator(
+            serialization_alias=lambda field_name: DC_PROPERTY_PREFIX + field_name,
+        )
+    )
+
+    name: Optional[str] = None
+    age: Optional[int] = None
+    spouse: Optional[UUID] = None
+    dog: Optional[UUID] = None
--- dataclay_restful/web/api/Person/views.py
+++ dataclay_restful/web/api/Person/views.py
@@ -1,3 +1,5 @@
+import json
+from typing import Any
 from fastapi import APIRouter, HTTPException
 
 
@@ -6,12 +8,18 @@ from uuid import UUID
 from fastapi import APIRouter
 from fastapi.param_functions import Depends
 from dataclay_restful.services.metadata_service.dependency import get_mds
-
+from dataclay_restful.services.dataclay.dependency import get_dc_client
+from dataclay_restful.web.api.Person.schema import PersonBase
 
 from dataclay.metadata.api import MetadataAPI
 
 from dataclay.metadata.kvdata import ObjectMetadata
 from dataclay.exceptions import DoesNotExistError
+from dataclay.contrib.modeltest.family import Person
+from dataclay.dataclay_object import DataClayObject
+
+from dataclay.config import session_var
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,20 +29,101 @@ router = APIRouter()
 @router.get("/")
 async def read_persons(mds: MetadataAPI = Depends(get_mds)) -> dict[UUID, ObjectMetadata]:
     """
-    Retrieve all persons.
+    Retrieve all persons metadata.
     """
     return await mds.get_all_objects(filter_func=lambda x: "Person" in x.class_name)
 
 
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, DataClayObject):
+            return obj.getID()
+        # elif isinstance(obj, UUID):
+        #     return str(obj)
+        return super().default(obj)
+
+
 @router.get("/{uuid}")
-async def read_person(id: UUID, mds: MetadataAPI = Depends(get_mds)) -> ObjectMetadata:
+async def read_person(id: UUID) -> Any:
     """
     Retrieve a person by UUID.
     """
+    # TODO: What should we do with unserializable objects? Maybe just return a pickled object?
+
     try:
-        return await mds.get_object_md_by_id(id)
+        person = Person.get_by_id(id)
+    except DoesNotExistError as e:
+        raise HTTPException(status_code=404, detail=f"Person with UUID {id} does not exist.")
+
+    try:
+        properties = person.get_properties()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to retrieve person properties.")
+    print(json.dumps(properties, cls=CustomEncoder, indent=2))
+    return json.dumps(properties, cls=CustomEncoder)
+
+
+@router.get("/{uuid}/{attribute}")
+async def read_person_attribute(id: UUID, attribute: str) -> Any:
+    """
+    Retrieve the value of an attribute for a person by UUID.
+    """
+    # TODO: Improve the session handling with contextvars
+    session_var.set(
+        {
+            "dataset_name": "admin",
+            "username": "admin",
+            "token": "admin",
+        }
+    )
+
+    try:
+        person = Person.get_by_id(id)
+    except DoesNotExistError as e:
+        raise HTTPException(status_code=404, detail=f"Person with UUID {id} does not exist.")
+
+    try:
+        attribute_value = getattr(person, attribute)
+    except AttributeError:
+        raise HTTPException(
+            status_code=400, detail=f"Attribute '{attribute}' does not exist in Person object."
+        )
+
+    # If the attribute is a DataClayObject, return its ID
+    # TODO: It will fail if the returned object is more complex, with nested DataClayObjects.
+    # Maybe use json/pickle to serialize the object?
+    if isinstance(attribute_value, DataClayObject):
+        return attribute_value.getID()
+    else:
+        return attribute_value
+
+
+@router.put("/{uuid}")
+async def update_person_attribute(id: UUID, update_request: PersonBase) -> Any:
+    """
+    Update the attributes of a person by UUID.
+    """
+    # TODO: Improve the session handling with contextvars
+    session_var.set(
+        {
+            "dataset_name": "admin",
+            "username": "admin",
+            "token": "admin",
+        }
+    )
+
+    try:
+        person = Person.get_by_id(id)
     except DoesNotExistError as e:
         raise HTTPException(status_code=404, detail=f"Person with UUID {id} does not exist.")
     except Exception as e:
-        # Log the exception if needed
         raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    try:
+        # TODO: Allow to update DataClayObject attributes using their UUID.
+        # We should first retrieve the object attribute by its UUID and then update the current object attribute.
+        person.dc_update_properties(update_request.model_dump(exclude_unset=True, by_alias=True))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to update attribute value.")
+
+    return {"message": "Attribute updated successfully."}
